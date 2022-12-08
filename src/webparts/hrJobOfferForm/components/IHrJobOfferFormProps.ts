52d3cb8b8fec5c2169b060dc187833340cfcf2d2
Steven Chorkawy <sc13@clarington.net>
2022-12-08 09:33:24 -0500

    Adding SPFx scaf

---
+++ src/webparts/hrJobOfferForm/components/IHrJobOfferFormProps.ts
@@ -0,0 +1,7 @@
+export interface IHrJobOfferFormProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/hrJobOfferForm/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IHrJobOfferFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'HrJobOfferFormWebPartStrings' {
+  const strings: IHrJobOfferFormWebPartStrings;
+  export = strings;
+}
