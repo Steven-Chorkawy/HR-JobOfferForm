d3357ced0e2346793910f888737cee49db359bab
Steven Chorkawy <schorkawy@clarington.net>
2022-12-09 12:08:05 -0500

    Testing PnP V3.

--- src/HelperMethods/MyHelperMethods.tsx
+++ src/HelperMethods/MyHelperMethods.tsx
@@ -1,3 +1,15 @@
+import { spfi } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/sites";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import "@pnp/sp/files";
+import "@pnp/sp/folders";
+import "@pnp/sp/security";
+import { MyLibraries } from "../enums/MyLibraries";
+import { getSP } from "../webparts/hrJobOfferForm/pnpjsConfig";
+
+
 
 // Title = JobID-Position-CandidateName
 export const FormatTitle = (JobID: string, Position: string, CandidateName: string): string => {
@@ -5,4 +17,26 @@ export const FormatTitle = (JobID: string, Position: string, CandidateName: stri
         return null;
 
     return `${JobID} - ${Position} - ${CandidateName}`;
+}
+
+// TODO: Get a list of documents in the template library.
+export const GetTemplateDocuments = async () => {
+
+    let sp = getSP();
+
+    let test = await spfi(sp).site.getDocumentLibraries('https://claringtonnet.sharepoint.com/sites/HR');
+    console.log('libraries:')
+    console.log(test);
+
+    let lists = await spfi(sp).web.lists()
+    console.log('lists');
+    console.log(lists);
+
+    let templateDocuments = await spfi(sp).web.lists.getByTitle(MyLibraries.JobOfferTemplatesLibrary).items.select("Id", "Title", "FileLeafRef", "File/Length").expand("File/Length")();
+  
+
+    console.log('Template Documents');
+    console.log(templateDocuments);
+
+    return templateDocuments;
 }
