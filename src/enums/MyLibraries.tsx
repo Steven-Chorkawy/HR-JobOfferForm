630836aedd1d8f979c1b7edfee225633d5d55490
Steven Chorkawy <schorkawy@gmail.com>
2022-12-14 14:46:51 -0500

    Updating library names and adding a content type ID.

--- src/enums/MyLibraries.tsx
+++ src/enums/MyLibraries.tsx
@@ -1,4 +1,5 @@
 export enum MyLibraries {
-    JobOffersLibrary = "JobOffers",
-    JobOfferTemplatesLibrary = "JobOfferTemplates"
+    JobOffersLibrary = "Job Offers",
+    JobOfferDocumentSetContentTypeID = "0x0120D52000FA20627A7220AA45BCB50141887C9FF8003B4B35F6659549428477E09873B3D8ED",
+    JobOfferTemplatesLibrary = "Job Offer Templates"
 }
